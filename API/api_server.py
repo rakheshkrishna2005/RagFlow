5fad420e2ca881ba60d2d7976fa7dab65986f2c4
rakheshkrishna2005 <rakheshkrishnap@gmail.com>
2025-08-31 16:35:14 +0530

    port config

--- API/api_server.py
+++ API/api_server.py
@@ -381,4 +381,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
